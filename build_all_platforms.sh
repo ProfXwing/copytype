266edda159ccff7edf905293e73e31c5c8ba6780
ProfXwing <legoxwing1@gmail.com>
2022-07-01 18:15:59 -0400

    better build scripts

--- build_all_platforms.sh
+++ build_all_platforms.sh
@@ -1,1 +1,16 @@
-npx electron-packager . --all --icon=icon.ico --out=out
+npx electron-packager . --icon=icon.ico --out=out --platform=win32 --arch=arm64 --ignore=Great_Expectations.epub
+npx electron-packager . --icon=icon.ico --out=out --platform=win32 --arch=x64 --ignore=Great_Expectations.epub
+npx electron-packager . --icon=icon.ico --out=out --platform=win32 --arch=ia32 --ignore=Great_Expectations.epub
+
+npx electron-packager . --icon=icon.ico --out=out --platform=darwin --arch=arm64 --ignore=Great_Expectations.epub
+npx electron-packager . --icon=icon.ico --out=out --platform=darwin --arch=x64 --ignore=Great_Expectations.epub
+
+npx electron-packager . --icon=icon.ico --out=out --platform=linux --arch=arm64 --ignore=Great_Expectations.epub
+npx electron-packager . --icon=icon.ico --out=out --platform=linux --arch=armv7l --ignore=Great_Expectations.epub
+npx electron-packager . --icon=icon.ico --out=out --platform=linux --arch=ia32 --ignore=Great_Expectations.epub
+npx electron-packager . --icon=icon.ico --out=out --platform=linux --arch=x64 --ignore=Great_Expectations.epub
+
+for entry in "out"/*
+do
+    cp Great_Expectations.epub $entry
+done
--- build_your_platform.sh
+++ build_your_platform.sh
@@ -1,1 +1,6 @@
-npx electron-packager . --icon=icon.ico --out=out
+npx electron-packager . --icon=icon.ico --out=out --ignore=Great_Expectations.epub
+
+for entry in "out"/*
+do
+    cp Great_Expectations.epub $entry
+done
