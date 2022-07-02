fb8a2eab5117eacc505f091bba4aace721d125de
ProfXwing <legoxwing1@gmail.com>
2022-07-01 22:04:47 -0400

    made building even better

--- build_all_platforms.sh
+++ build_all_platforms.sh
@@ -1,16 +1,19 @@
-npx electron-packager . --icon=icon.ico --out=out --platform=win32 --arch=arm64 --ignore=Great_Expectations.epub
-npx electron-packager . --icon=icon.ico --out=out --platform=win32 --arch=x64 --ignore=Great_Expectations.epub
-npx electron-packager . --icon=icon.ico --out=out --platform=win32 --arch=ia32 --ignore=Great_Expectations.epub
+# im really bad at shell but this should work well enough.
+rm -r out
+npx electron-packager . --icon=icon.ico --out=out --platform=win32,darwin,linux --arch=ia32,x64,arm64,armv7l --ignore=Great_Expectations.epub
 
-npx electron-packager . --icon=icon.ico --out=out --platform=darwin --arch=arm64 --ignore=Great_Expectations.epub
-npx electron-packager . --icon=icon.ico --out=out --platform=darwin --arch=x64 --ignore=Great_Expectations.epub
+cd out
 
-npx electron-packager . --icon=icon.ico --out=out --platform=linux --arch=arm64 --ignore=Great_Expectations.epub
-npx electron-packager . --icon=icon.ico --out=out --platform=linux --arch=armv7l --ignore=Great_Expectations.epub
-npx electron-packager . --icon=icon.ico --out=out --platform=linux --arch=ia32 --ignore=Great_Expectations.epub
-npx electron-packager . --icon=icon.ico --out=out --platform=linux --arch=x64 --ignore=Great_Expectations.epub
+finalize () {
+    cp ../Great_Expectations.epub $1
+    zip -r $1.zip $1 
+    rm -r $1
+    echo "Finished building $1"
+}
 
-for entry in "out"/*
+for entry in ./*
 do
-    cp Great_Expectations.epub $entry
+    finalize $entry &
 done
+
+# if anyone knows how to make sure the the program is done before ending (letting you type in the terminal), please fix
--- build_your_platform.sh
+++ build_your_platform.sh
@@ -1,6 +1,12 @@
+rm -r out
 npx electron-packager . --icon=icon.ico --out=out --ignore=Great_Expectations.epub
 
-for entry in "out"/*
+cd out
+
+for entry in ./*
 do
-    cp Great_Expectations.epub $entry
+    cp ../Great_Expectations.epub $entry
+    zip -r $entry.zip $entry 
+    rm -r $entry
+    echo "Finished building $entry"
 done
